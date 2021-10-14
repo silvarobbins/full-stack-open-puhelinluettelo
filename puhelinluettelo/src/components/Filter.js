6a4be0d32dba16b5ed541cf01fb0002e940cd180
Silva Robbins <silva.robbins@gmail.com>
2021-10-14 16:21:14 +0300

    reset

--- puhelinluettelo/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({filter, handleFilterChange}) => {
-    return(
-    <div>
-        Filter shown with: <input value={filter} onChange={handleFilterChange} />
-    </div>
-    )
-}
-
-export default Filter
