1193c24b615093b0a319917d0c07d3e98e0d68e1
Silva Robbins <silva.robbins@gmail.com>
2021-10-26 20:32:41 +0300

    3.12

--- index.js
+++ index.js
@@ -18,29 +18,6 @@ app.get('/', function (req, res) {
     res.send('hello, world!')
   })
 
-let persons = [
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456",
-    },
-    {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-    },
-    {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345",
-    },
-    {  
-      id: 4,
-      name: "Mary Poppendick",
-      number: "39-23-6423122",
-    }
-  ]
-
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
